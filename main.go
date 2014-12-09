5dffa209cd6d46bd9c8123851ef0b3ec44a57a62
Chakrit Wichian <service@chakrit.net>
2014-12-09 13:35:04 +0700

    Initial JSONRPC experiment.

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"os"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+type TestService struct{}
+
+type HelloArg struct{ Name string }
+type HelloResult struct { Line string }
+
+func (ts *TestService) Hello(args *HelloArg, result *HelloResult) error {
+	*result = HelloResult{
+		Line: "hello, " + args.Name,
+	}
+	return nil
+}
+
+func main() {
+	switch os.Args[1] {
+	case "client":
+		startClient()
+	case "server":
+		startServer()
+	default:
+		log.Fatal("specify either `server` or `client` mode.")
+	}
+}
+
+func startClient() {
+	client, e := jsonrpc.Dial("tcp", "0.0.0.0:7331")
+	noError(e)
+
+	arg := &HelloArg{"Chakrit"}
+	line := &HelloResult{}
+	e = client.Call("TestService.Hello", arg, line)
+	noError(e)
+
+	log.Println(line.Line)
+}
+
+func startServer() {
+	log.Println("starting server...")
+	server := rpc.NewServer()
+	server.Register(&TestService{})
+
+	listener, e := net.Listen("tcp", "0.0.0.0:7331")
+	noError(e)
+
+	for {
+		conn, e := listener.Accept()
+		noError(e)
+
+		log.Println("processing request...")
+		go func() {
+			server.ServeCodec(jsonrpc.NewServerCodec(conn))
+			log.Println("done.")
+		}()
+	}
+}
+
+func noError(e error) {
+	if e != nil {
+		log.Println("wait...")
+	}
+}
