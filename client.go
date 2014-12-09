3eb3637c9f58ae8d94536778ba5fa14bd1cffd06
Chakrit Wichian <service@chakrit.net>
2014-12-09 17:55:32 +0700

    Use struct for the HELLO method.

--- client.go
+++ client.go
@@ -19,8 +19,9 @@ func Dial(network, address string) (LyncRPC, error) {
 }
 
 func (client *client) Hello(name string) (string, error) {
+	request := &struct{ Name string }{name}
 	response := ""
-	e := client.Call("HELLO", []string{name}, &response)
+	e := client.Call("HELLO", request, &response)
 	return response, e
 }
 
